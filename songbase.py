0a81be3dd46ca34e8e6ca222c2d95279a646297b
Yue Feng <yueyueer@yahoo.com>
2017-11-09 10:51:02 -0500

    new files

--- songbase.py
+++ songbase.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>hello world!!!!</h1>'
+    # return '<h1>hello world!!!!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<string:name>/')
+def get_user(name):
+    # return 'hello %s %d' % (name,3)
+    return render_template('user.html', user_name=name)
+
+@app.route('/my-song')
 def get_user():
-    return 'hello %s %d' % (name,3)
+    return render_template('my-song.html', user_name=name)
+
+
+@app.route('/songs')
+def get_all_songs():
+    songs = [
+        'song1',
+        'song2',
+        'song3'
+    ]
+    return render_template('songs.html', songs=songs)
 
 
 @app.route('/users')
