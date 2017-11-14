90d9daec27bf433b5998c0b6292f45dbf52ed1f3
Yue Feng <yueyueer@yahoo.com>
2017-11-14 10:48:02 -0500

    web form

--- songbase.py
+++ songbase.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY']='lalala'
 
 
 @app.route('/')
 def index():
     # return '<h1>hello world!!!!</h1>'
     return render_template('index.html')
+
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    if request.method == 'GET':
+        first_name = request.args.get('first_name') #this is for GET
+        if first_name:
+            return render_template('form-demo.html', first_name = first_name)
+        else:
+            return render_template('form-demo.html', first_name = session.get('first_name'))
+
+    if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        return redirect(url_for('form_demo'))
 
 @app.route('/user/<string:name>/')
 def get_user(name):
