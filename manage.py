25fc7e099b37ab150f8cb2f6a4573fd432874c5f
Yue Feng <yueyueer@yahoo.com>
2017-11-16 14:04:02 -0500

    new file

--- manage.py
+++ manage.py
@@ -1,7 +1,20 @@
 from flask_script import Manager
-from songbase import app
+from songbase import app, db, Artist, Song
 
 manager = Manager(app)
+
+# reset the database and create two artists
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    coldplay = Artist(name='Coldplay', about='Coldplay is a British rock band.')
+    maroon5 = Artist(name='Maroon 5', about='Maroon 5 is an American pop rock band.')
+    song1 = Song(name="Yellow", year=2004,lyrics='bla bla', artist = coldplay)
+    db.session.add(coldplay)
+    db.session.add(maroon5)
+    db.session.add(song1)
+    db.session.commit()
 
 if __name__=='__main__':
     manager.run()
--- songbase.py
+++ songbase.py
@@ -1,12 +1,65 @@
+import os
 from flask import Flask, render_template, session, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY']='lalala'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+# define database tables
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    about = db.Column(db.Text)
+    songs = db.relationship('Song', backref='artist')
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    year = db.Column(db.Integer)
+    lyrics = db.Column(db.Text)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+
 
 
 @app.route('/')
 def index():
     # return '<h1>hello world!!!!</h1>'
     return render_template('index.html')
+
+@app.route('/artists')
+def show_all_artists():
+    artists = Artist.query.all()
+    return render_template('artist-all.html', artists=artists)
+
+# song-all.html adds song id to the edit button using a hidden input
+@app.route('/songs')
+def show_all_songs():
+    songs = Song.query.all()
+    return render_template('song-all.html', songs=songs)
+
+
+@app.route('/artist/add', methods=['GET', 'POST'])
+def add_artists():
+    if request.method == 'GET':
+        return render_template('artist-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        about = request.form['about']
+
+        # insert the data into the database
+        artist = Artist(name=name, about=about)
+        db.session.add(artist)
+        db.session.commit()
+        return redirect(url_for('show_all_artists'))
+
 
 @app.route('/form-demo', methods=['GET', 'POST'])
 def form_demo():
